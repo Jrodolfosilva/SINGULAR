8b70dfcd4a6748e1c6c2d26c69568d606d5915a8
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2023-09-04 11:30:19 -0300

    feat: cards dinamic js

--- scripts/app.js
+++ scripts/app.js
@@ -23,4 +23,66 @@ ReqImoveis()
 
 
 
+/*
+
+Aqui criaremos os cards/elementos-html de forma dinâmica com js
+
+*/
+
+function createCards (result){
+
+    
+    const containerCard = document.querySelector(".container_immobile__container_carrossel") ;/*Div onde ficará todos meus imóveis */
+ 
+    
+ 
+    result&&result.map((imovel)=>{
+ 
+         const cardEnterprise =  document.createElement("div");
+
+         const title = document.TEXT_NODE = imovel.title.rendered;
+         const endereco =  document.TEXT_NODE = imovel.acf.endereco;
+         const vagas = document.TEXT_NODE = imovel.acf.vagas;
+         const area =  document.TEXT_NODE = imovel.acf.area_construida +" m<sup>2</sup>"
+         const image = document.textContent = `http://singular-online-wp.local/wp-admin/upload.php?item=${imovel.acf.imagem}`
+        
+
+         const titleElement = document.createElement('h1')
+         titleElement.innerHTML=title;
+        
+         const enderecoElement = document.createElement('p')
+         enderecoElement.innerHTML=endereco;
+        
+         const areaElement = document.createElement('p')
+         areaElement.innerHTML=area;
+
+         const vagasElement = document.createElement('p')
+         vagasElement.innerHTML=vagas;
+        
+        
+         cardEnterprise.classList.add("card")/*Adicionar uma class nos cards */
+         cardEnterprise.appendChild(titleElement)
+         cardEnterprise.appendChild(enderecoElement)
+         cardEnterprise.appendChild(areaElement)
+         cardEnterprise.appendChild(vagasElement)
+         cardEnterprise.style.backgroundImage="url(http://singular-online-wp.local/wp-content/uploads/2023/09/unibanco.jpg)"
+         cardEnterprise.style.backgroundSize='cover'
+         cardEnterprise.style.backgroundRepeat='no-repeat'
+         containerCard.appendChild(cardEnterprise)/*Adicionar os cards na div container */
+
+
+         
+ 
+         console.log(imovel)
+         
+ 
+ 
+ 
+ 
+ })
+ 
+ 
+ 
+ }
+ createCards()
 
