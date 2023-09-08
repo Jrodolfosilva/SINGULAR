8fb63d526ba02c98d1285111627570fc046d40fb
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2023-09-08 20:24:31 -0300

    fix: fetch image

--- scripts/app.js
+++ scripts/app.js
@@ -1,91 +1,69 @@
-
-
 /*
 Javascript para realização da requisição aos imóveis.
-
 */
-const baseurl = "https://spendstore.shop/wp-json/wp/v2/imovel"
+const baseurl = "https://spendstore.shop/wp-json/wp/v2/imovel";
 
-async function ReqImoveis(){
+async function ReqImoveis() {
     let result;
 
-    const response = await fetch(baseurl).catch((error)=>console.log(error))
-    result = await response.json()
+    const response = await fetch(baseurl).catch((error) => console.log(error));
+    result = await response.json();
 
-    if(result){
-        createCards(result)
+    if (result) {
+        createCards(result);
     }
-
-
 }
 
-ReqImoveis()
-
-
+ReqImoveis();
 
 /*
-
 Aqui criaremos os cards/elementos-html de forma dinâmica com js
-
 */
+async function createCards(result) {
+    const containerCard = document.querySelector(".container_immobile__container_carrossel"); /*Div onde ficará todos meus imóveis */
 
-function createCards (result){
+    await Promise.all(
+        result.map(async (imovel) => {
+            const imagem = await fetch(`https://spendstore.shop/wp-json/wp/v2/media?parent=${imovel.id}`)
+                .then((resp) => resp.json())
+                .catch((err) => console.log(err));
 
-    
-    const containerCard = document.querySelector(".container_immobile__container_carrossel") ;/*Div onde ficará todos meus imóveis */
- 
-    
- 
-    result&&result.map((imovel)=>{
- 
-         const cardEnterprise =  document.createElement("div");
+            const cardEnterprise = document.createElement("div");
+            const title = imovel.title.rendered;
+            const endereco = imovel.acf.endereco;
+            const vagas = imovel.acf.vagas;
+            const area = imovel.acf.area_construida + " m<sup>2</sup>";
 
-         const title = document.TEXT_NODE = imovel.title.rendered;
-         const endereco =  document.TEXT_NODE = imovel.acf.endereco;
-         const vagas = document.TEXT_NODE = imovel.acf.vagas;
-         const area =  document.TEXT_NODE = imovel.acf.area_construida +" m<sup>2</sup>"
-         const image = document.textContent = `http://singular-online-wp.local/wp-admin/upload.php?item=${imovel.acf.imagem}`
-        
+            const titleElement = document.createElement('h1');
+            titleElement.innerHTML = title;
 
-         const titleElement = document.createElement('h1')
-         titleElement.innerHTML=title;
-        
-         const enderecoElement = document.createElement('p')
-         enderecoElement.innerHTML=endereco;
-        
-         const areaElement = document.createElement('p')
-         areaElement.innerHTML=area;
+            const enderecoElement = document.createElement('p');
+            enderecoElement.innerHTML = endereco;
 
-         const vagasElement = document.createElement('p')
-         vagasElement.innerHTML=vagas;
-        
-         const hoverElement =  document.createElement("div")
-         hoverElement.classList.add('hover')
-        
-         cardEnterprise.classList.add("card")/*Adicionar uma class nos cards */
-         hoverElement.appendChild(titleElement)
-         hoverElement.appendChild(enderecoElement)
-         hoverElement.appendChild(areaElement)
-         hoverElement.appendChild(vagasElement)
-         cardEnterprise.appendChild(hoverElement)
-         cardEnterprise.style.backgroundImage="url(https://spendstore.shop/wp-content/uploads/2023/09/unibanco.jpg)"
-         cardEnterprise.style.backgroundSize='cover'
-         cardEnterprise.style.backgroundRepeat='no-repeat'
-         containerCard.appendChild(cardEnterprise)/*Adicionar os cards na div container */
+            const areaElement = document.createElement('p');
+            areaElement.innerHTML = `${area} | ${vagas == 0 || vagas == 1 ? `${vagas} Vaga` : `${vagas} Vagas`}`;
 
+            const hoverElement = document.createElement("div");
+            hoverElement.classList.add('hover');
 
-         
- 
-         console.log(imovel)
-         
- 
- 
- 
- 
- })
- 
- 
- 
- }
- createCards()
+            cardEnterprise.classList.add("card"); /*Adicionar uma class nos cards */
 
+            if (imagem)  imagem.map((obj)=>{
+                
+                cardEnterprise.style.backgroundImage = `url(${obj.guid.rendered})`
+            
+            })
+            cardEnterprise.style.backgroundSize = 'cover';
+            cardEnterprise.style.backgroundRepeat = 'no-repeat';
+
+           
+
+            hoverElement.appendChild(titleElement);
+            hoverElement.appendChild(enderecoElement);
+            hoverElement.appendChild(areaElement);
+
+            cardEnterprise.appendChild(hoverElement);
+            containerCard.appendChild(cardEnterprise); /*Adicionar os cards na div container */
+        })
+    );
+}
