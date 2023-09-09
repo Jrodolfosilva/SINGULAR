49b47598325742b88693e23a5cb3c557e222070b
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2023-09-08 23:53:07 -0300

    feat: map google, js page empreendimento content dinamic

--- scripts/fetchImovel.js
+++ scripts/fetchImovel.js
@@ -1,6 +1,4 @@
-
 const url = new URL(window.location.href);
-
 
 const params = new URLSearchParams(url.search);
 
@@ -8,38 +6,60 @@ const codValue = params.get('cod');
 
 // Verifique se o valor foi encontrado
 if (codValue !== null) {
-    details(codValue)
+    details(codValue);
     console.log('Valor do parâmetro "cod":', codValue);
 } else {
     console.log('O parâmetro "cod" não foi encontrado na URL.');
 }
 
+async function details(codigo) {
+    try {
+        const imovel = await fetch(`https://spendstore.shop/wp-json/wp/v2/imovel/${codigo}`);
+        const details = await imovel.json();
+
+        const imagem = await fetch(`https://spendstore.shop/wp-json/wp/v2/media?parent=${codigo}`);
+        const imagemImovel = await imagem.json();
+
+        AdddetailsProperty(details, imagemImovel);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function AdddetailsProperty(details, imagemImovel) {
+    const titleProperty = document.querySelector(".title_property");
+    const image_property = document.querySelector(".image_property");
+    const description_property = document.querySelector(".description_property");
+    const codigo_property = document.querySelector(".codigo_property");
+    const address_property = document.querySelector(".address_property");
+    const address_property2 = document.querySelector(".address_property2")
+    const areaProperty = document.querySelector(".areaProperty")
+    const map =  document.querySelector('#map')
 
 
-async function details(codigo){
+
+    const title =  details.title.rendered;
+    const address = details.acf.endereco;
+    const description = details.content.rendered;
+    const area = details.acf.area_construida + " m<sup>2</sup>"
+    const vagas = details.acf.vagas
+    const mapGoogle = `https://www.google.com.br/maps?q=${encodeURIComponent(address)}
+  ,%20Brasil&output=embed`
     
-   try{
+    let imagem = "";
 
-    const imovel = await fetch(`https://spendstore.shop/wp-json/wp/v2/imovel/${codigo}`)
-    const details = await  imovel.json()
-    AdddetailsProperty(details)
-   }
-   catch(error){
-    console.log(error)
-   }
+    if (imagemImovel) {
+        imagem = imagemImovel.map((obj) => obj.guid.rendered);
+    }
 
-
-}
-
-
-async function AdddetailsProperty(details){
-
-const title =  details.title;
-const address =  details.endereco;
-const description = details.descricao;
-const codigo = codValue;
-
+    titleProperty.innerHTML = title;
+    address_property.innerHTML = address;
+    description_property.innerHTML = description; 
+    image_property.setAttribute('src', `${imagem}`);
+    codigo_property.innerHTML = `Código do imóvel: ${codValue}`;
+    address_property2.innerHTML = address
+    areaProperty.innerHTML = `${area} | ${vagas == 0 || vagas == 1 ? `${vagas} Vaga` : `${vagas} Vagas`}`;
+    map.setAttribute('src',`${mapGoogle}`)
 
 
 }
-
