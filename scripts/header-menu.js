24784cf9f9216f9b090d479093325c72356691ed
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2023-09-09 21:32:16 -0300

    style: menu mobile

--- scripts/header-menu.js
+++ scripts/header-menu.js
@@ -1,7 +1,9 @@
 const button =  document.querySelector("#toggle-menu");
+
 const menu = document.querySelector(".menu")
 
 button.addEventListener('click',()=>{
     
-    menu.classList.toggle('ativo')
+    menu.classList.toggle('ativo');
+    button.classList.toggle('bg-js')
 })
